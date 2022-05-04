da271206f97c831940f2ef2eb344c922321c3355
GalMarkelj <galmarkelj09@gmail.com>
2022-05-04 09:18:46 +0200

    Create migration aka -database-

---
+++ migrations/20220504071411_authentication-app.js
@@ -0,0 +1,43 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.createTable('user', (tbl) => {
+        tbl.increments()
+        tbl.text('username')
+            .notNullable()
+            .unique()
+        tbl.text('email')
+            .notNullable()
+            .unique()
+        tbl.text('password')
+            .notNullable
+        tbl.timestamps(true, true)
+    })
+
+    .createTable('user_data', (tbl) => {
+        tbl.increments()
+        tbl.text('sex')
+        tbl.text('address')
+        tbl.text('town')
+        tbl.integer('phone_number')
+
+        //Foreign key to 'user' table
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('user')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('user_data').dropTableIfExists('user')
+};
